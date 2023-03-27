cd21b8ed4f4547db18083076ab54c6c4b8c326a4
csjaybit <118171209+csjaybit@users.noreply.github.com>
2023-03-27 15:59:31 +0530

    Update calculator.sol

--- calculator.sol
+++ calculator.sol
@@ -2,16 +2,16 @@
 pragma solidity 0.8.18;
 
 contract Calculator{
-    function add(uint a,uint b)external pure returns(uint){
+    function addition(uint a,uint b)external pure returns(uint){
         return a+b;
     }
-    function sub(int a,int b)external pure returns(int){
+    function subtract(int a,int b)external pure returns(int){
         return a-b;
     }
-    function mult(uint a,uint b)external pure returns(uint){
+    function multiplication(uint a,uint b)external pure returns(uint){
         return a*b;
     }
-    function div(uint a,uint b)external pure returns(uint){
+    function division(uint a,uint b)external pure returns(uint){
         return a/b;
     }
 
